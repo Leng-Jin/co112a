9b46f66f0fdef24b5c9dc7f4dc47e3ca8a8b0836
Leng-Jin <s111110554@student.nqu.edu.tw>
2024-01-05 11:25:43 +0800

    all data

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,50 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@place // 起始點
+  M=0 // 放置在左上角
+
+(LOOP)
+  @KBD // 讀取鍵盤值
+  D=M // 將其放入 D
+  @WHITE
+  D;JEQ // 如果鍵盤值 == 0（未按下） 轉到 WHITE
+  @BLACK
+  0;JMP // 否則，轉到 BLACK
+
+(WHITE)
+  @place
+  D=M // 將 place 放入 D
+  @LOOP
+  D;JLT // 如果我們在最小值（0）以下，跳轉到 LOOP
+  @place
+  D=M
+  @SCREEN
+  A=A+D // 計算在屏幕上的位置
+  M=0 // 填充像素為白色
+  @place
+  M=M-1 // 減少 place
+  @LOOP
+  0;JMP // 跳回 LOOP
+
+(BLACK)
+  @place
+  D=M
+  @8192 // place 的最大值
+  D=D-A
+  @LOOP
+  D;JGE // 如果我們在最大值（place - 8192 >= 0）以上，跳轉到 LOOP
+  @place
+  D=M
+  @SCREEN
+  A=A+D // 計算位置
+  M=-1 // 填充像素為黑色
+  @place
+  M=M+1 // 將 place 增加 1
+  @LOOP
+  0;JMP // 跳回 LOOP
+
+(END)
+  @END
+  0;JMP // 無窮循環在結尾
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -5,5 +5,34 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//
+// This program only needs to handle arguments that satisfy
+// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@2
+    M=0     // 將R2設置為0
+    @i
+    M=0     // 將i設置為0
+
+(LOOP)
+    @i
+    D=M     // D存儲i的值
+    @0
+    D=D-M   // D等於i減去R0
+    @END
+    D;JGE    // 如果i-R0大於等於0，則跳轉到END標籤
+
+    @1
+    D=M     // D存儲R1的值
+    @2
+    M=D+M   // 將R2設置為R2加上R1的值
+    @i
+    M=M+1   // 將i加1
+    @LOOP
+    0;JMP   // 跳轉到LOOP標籤，進行下一輪迴圈
+
+(END)
+    @END
+    0;JMP   // 跳轉到END標籤，結束程序
